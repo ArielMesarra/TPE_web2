2d811b1c4a6c79ebd3540a74b4261cd310e7f394
ArielMesarra <arielmesarra28@gmail.com>
2022-09-27 18:32:44 -0300

    hicimos ruteo y header

---
+++ router.php
@@ -0,0 +1,30 @@
+<?php
+require_once('./templates/header.php');
+
+define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
+
+//recibir/leer la accion
+if (!empty($_GET['action'])) {
+    $accion = $_GET['action'];
+} else {
+    $accion = 'inicio';
+}
+
+// parseo el string de action por "/" y me devuelve el arreglo
+// $accion = "/tabla/asd" me llega en la variable "$accion" como un string
+
+$params = explode('/', $accion);
+// $params = ['tabla','asd']
+
+switch ($params[0]) {
+    case 'biblioteca':
+        echo '<h1>Biblioteca</h1>' ;
+        break;
+    case 'privado':
+        echo '<h1>Privado</h1>' ; 
+        break;
+    case 'inicio':
+    default:
+        echo '<h1>Inicio</h1>';
+        break;
+}
